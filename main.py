67b931d2f6d6eef10ef9ca270c2f2a1bb8cbe1bf
Khurshed <hurshed@gmail.com>
2025-01-07 22:28:40 +0500

    int(input)

--- main.py
+++ main.py
@@ -1,4 +1,36 @@
-n = 5
-m = "Five"
-print(type(n))
-print(type(m))
+# n = 5
+# m = "Five"
+# print(type(n))
+# print(type(m)) Функция type() показывает в терминале тип переменной
+# --------------------
+# a = 5
+# b = 1.23
+# c = "hello"
+# print(f"{a} - {b} - {c}")
+# print(type(a))
+# print(type(b))
+# print(type(c))
+# print(a, b, c, sep=" - ")
+# ---------------------------
+# print("Введите первую строку:")
+# a = input()
+# b = input("Введите вторую строку: ")  # Здесь в аргументе функции input() мы ввели текст, который выходит в одной
+# # строке и нигде не сохранятеся также как и функция print()
+# print(b)
+# -----------------------
+# c = 5.89
+# print(c)
+# n = int(c)
+# print(n)
+
+# c = 5.89
+# print(c)
+# print(type(c))
+# c = int(c)
+# print(c)
+# print(type(c))
+
+print("Введите первое число: ")
+a = int(input())
+b = int(input("Введите второе число: "))
+print(a, " + ", b, " = ", a + b)
